ea94ce890e177244f0f6b843d383ab18f76ebfff
hfmsolucoes <hfm.solucoes@hotmail.com>
2019-08-03 19:04:11 -0300

    Criando Layout e Global styled

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import { Router } from 'react-router-dom';
 import history from './services/history';
 import Routes from './routes';
 import './config/ReactotronConfig';
+import GlobalStyled from './styles/global';
+
 function App() {
   return (
     <Router history={history}>
       <Routes />
+      <GlobalStyled />
     </Router>
   );
 }
