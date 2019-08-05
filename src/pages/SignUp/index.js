c9fb24ebfee4602556e3b41ccbcbb6ae30c1557e
Flavio Martins da Silva <flavio.martins@ormecmvr.net>
2019-08-05 16:08:21 -0300

    Implementação do Unform

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <form>
-        <input placeholder="Nome completo" />
-        <input type="email" placeholder="Seu e-mail aqui" />
-        <input type="password" placeholder="Sua senha aqui" />
+      <Form onSubmit={handleSubmit}>
+        <Input placeholder="Nome completo" />
+        <Input type="email" placeholder="Seu e-mail aqui" />
+        <Input type="password" placeholder="Sua senha aqui" />
         <button type="submit">Criar conta</button>
         <Link to="/">Já tenho login</Link>
-      </form>
+      </Form>
     </>
   );
 }
