c49394f35c4929ec46368094da8e1e5959d92c48
Flavio Martins da Silva <flavio.martins@ormecmvr.net>
2019-08-06 16:36:37 -0300

    Configurando Redux

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import history from './services/history';
 import Routes from './routes';
 import './config/ReactotronConfig';
+import store from './store';
 import GlobalStyled from './styles/global';
 
 function App() {
   return (
-    <Router history={history}>
-      <Routes />
-      <GlobalStyled />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyled />
+      </Router>
+    </Provider>
   );
 }
 
