7db0f33134a76fdfcbc7c646cf64237b3f464670
Flavio Martins da Silva <flavio.martins@ormecmvr.net>
2019-08-08 16:01:17 -0300

    Cadastro de usuário

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -15,7 +15,8 @@ export function* signIn({ payload }) {
     const { token, user } = response.data;
 
     if (!user.provider) {
-      toast.error('Usuário não é um prestador.');
+      toast.error('Usuário não é um provider!');
+      throw new Error();
     }
 
     yield put(signInSuccess(token, user));
@@ -26,4 +27,24 @@ export function* signIn({ payload }) {
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+    history.push('/');
+  } catch (err) {
+    console.tron.error(err);
+    toast.error('Falha no cadastro, verifique seus dados');
+    yield put(signFailure());
+  }
+}
+
+export default all([
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
