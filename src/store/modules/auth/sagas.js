ebc229c8ca9751fbfd2b3604793a51709fb4c0f3
Flavio Martins da Silva <flavio.martins@ormecmvr.net>
2019-08-08 16:44:53 -0300

    Authorization implementado

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -18,7 +18,7 @@ export function* signIn({ payload }) {
       toast.error('Usuário não é um provider!');
       throw new Error();
     }
-
+    api.defaults.headers.Authorization = `Bearer ${token}`;
     yield put(signInSuccess(token, user));
     history.push('/dashboard');
   } catch (err) {
@@ -44,7 +44,17 @@ export function* signUp({ payload }) {
   }
 }
 
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
 export default all([
+  takeLatest('persist/REHYDRATE', setToken),
   takeLatest('@auth/SIGN_IN_REQUEST', signIn),
   takeLatest('@auth/SIGN_UP_REQUEST', signUp),
 ]);
