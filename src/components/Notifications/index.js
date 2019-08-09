642360569bb1a5e8067f74d9ed45a6a1a35b141e
Flavio Martins da Silva <flavio.martins@ormecmvr.net>
2019-08-09 17:29:52 -0300

    notificações

--- src/components/Notifications/index.js
+++ src/components/Notifications/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { MdNotifications } from 'react-icons/md';
+import api from '~/services/api';
 import {
   Container,
   Badge,
@@ -9,13 +10,26 @@ import {
 } from './styles';
 
 export default function Notifications() {
+  const [visible, setVisible] = useState(false);
+  const [notifications, setNotifications] = useState([]);
+
+  useEffect(() => {
+    async function loadNotification() {
+      const response = await api.get('notifications');
+      setNotifications(response.data);
+    }
+  });
+  function handleToggleVisible() {
+    setVisible(!visible);
+  }
+
   return (
     <Container>
-      <Badge hasUnread>
+      <Badge onClick={handleToggleVisible} hasUnread>
         <MdNotifications size={20} color="#7159c1" />
       </Badge>
 
-      <NotificationList>
+      <NotificationList visible={visible}>
         <Scroll>
           <Notification unread>
             <p>Você possui um novo agendamento</p>
