3e98ea86e21264f97d7c85f9c186aa8839c740e8
Flavio Martins da Silva <flavio.martins@ormecmvr.net>
2019-08-12 14:07:15 -0300

    Carregamento automático do formulário

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Form, Input } from '@rocketseat/unform';
+import { useSelector } from 'react-redux';
 import { Container } from './styles';
 
 export default function Profile() {
+  const profile = useSelector(state => state.user.profile);
+  function handleSubmit(data) {}
   return (
     <Container>
-      <Form>
+      <Form initialData={profile} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Nome Completo" />
         <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
         <hr />
